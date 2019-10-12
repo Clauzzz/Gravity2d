274676e4f066224191186ec9cee2cfaea8449443
claudio <claudiu.cuciurean@yahoo.com>
2019-10-12 10:30:49 +0300

    Bug in collisions fixed

--- frontend/scripts/physics/main.js
+++ frontend/scripts/physics/main.js
@@ -33,7 +33,7 @@ function removeObjectFromAll(objectToRemove)
     {
         for(let j=0; j<objects.listenerObjects[i].objectsToListen.length;j++)
         {
-            if(objects.listenerObjects[i].objectsToListen[j])
+            if(objects.listenerObjects[i].objectsToListen[j].id == objectToRemove.id)
             {
                 objects.listenerObjects[i].objectsToListen.splice(j,1);
                 j--;
@@ -45,16 +45,16 @@ function initialize()
 {
     let space = new Canvas('space');
     let universe = new Universe();
-    /*for(let i=0;i<10;i++)
+    for(let i=0;i<10;i++)
     {
         let asteroid = new Asteroid(i);
         asteroid.setMass(Math.random()*2+5);
-        asteroid.setDensity(Math.random()*0.001);
+        asteroid.setDensity(Math.random()*0.01);
         asteroid.setColor('rgb('+Math.floor(Math.random()*255)+','+Math.floor(Math.random()*255)+','+Math.floor(Math.random()*255)+')');
         asteroid.setPositionX(Math.random()*space.width);
         asteroid.setPositionY(Math.random()*space.height);
-    }*/
-    let asteroid1 = new Asteroid(1);
+    }
+    /*let asteroid1 = new Asteroid(1);
     let asteroid2 = new Asteroid(2);
     asteroid1.setMass(Math.random()*2+5);
     asteroid1.setDensity(Math.random()*0.001);
@@ -67,7 +67,7 @@ function initialize()
     asteroid2.setColor('rgb('+Math.floor(Math.random()*255)+','+Math.floor(Math.random()*255)+','+Math.floor(Math.random()*255)+')');
     asteroid2.setPositionX(700);
     asteroid2.setPositionY(500);
-
+    */
     space.setListenerObjects(objects);
     universe.setListenerObjects(objects);
 
